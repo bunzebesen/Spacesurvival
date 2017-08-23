5ec58014c5359c5dc4958e9dc7d3a4d1b8493ab3
Timo Küster <timo_kuester@web.de>
2017-08-23 18:08:40 +0200

    Server Einstellungen verbessert

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,23 @@ module.exports = function(grunt) {
 
 	// Project configuration. 
 	grunt.initConfig({
-	  concat: {
-	    dist: {
-	      src: ['js/game.js', 'js/resources.js', 'js/entities/entities.js', 'js/entities/HUD.js', 'js/screens/play.js'],
-	      dest: 'build/scripts.js',
+	  copy: {
+	    build: {
+	      files: [
+	        // includes files within path and its sub-directories 
+	        {expand: true, src: ['js/**'], dest: 'build/'},
+	        {expand: true, src: ['data/**'], dest: 'build/'},
+
+	      ],
 	    },
 	  },
 
 	  watch: {
 		scripts: {
-		  files: ['js/**/*.js'],
-	      tasks: ['concat'],
+		  files: ['js/**/*.js', 'data/**/*.json',
+		  		  'data/**/*.tmx',
+		  		  'data/**/*.png'],
+	      tasks: ['copy'],
 	    },
 	  },
 
@@ -25,9 +31,9 @@ module.exports = function(grunt) {
       },
     });
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 
-    grunt.registerTask('serve', ['concat', 'connect', 'watch']);
+    grunt.registerTask('server', ['copy', 'connect', 'watch']);
 };
