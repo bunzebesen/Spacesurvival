bdda43972f5f9e42598e21444ada8a34fe18bfe5
Timo Küster <timo_kuester@web.de>
2017-08-24 21:48:58 +0200

    highscore und textfield eingebaut

--- js/screens/score.js
+++ js/screens/score.js
@@ -6,7 +6,8 @@ game.ScoreScreen = me.ScreenObject.extend({
 
         if (me.device.isMobile) {
             this.UI.addChild(new game.UI.Button(0,                          0, me.game.viewport.width / 2, me.game.viewport.height, me.input.KEY.ESC));            
-            this.UI.addChild(new game.UI.Button(me.game.viewport.width / 2, 0, me.game.viewport.width / 2, me.game.viewport.height, me.input.KEY.ENTER));               
+            this.UI.addChild(new game.UI.Button(me.game.viewport.width / 2, 0, me.game.viewport.width / 2, me.game.viewport.height, me.input.KEY.ENTER));       
+            this.UI.addChild(new game.GUI.TextField(me.game.viewport.width / 2, 400, "text", 600));        
         }
 
 
@@ -25,31 +26,33 @@ game.ScoreScreen = me.ScreenObject.extend({
 
                 var font = new me.Font('monospace', 45, 'white');
 
-                this.widthItem = 0;
+//                this.widthItem = 0;
 
                 this.text1 = "YOUR SCORE: " + game.data.score;
                 this.text2 = "PRESS ESCAPE TO EXIT";
                 this.text3 = "PRESS ENTER TO PLAY AGAIN";
                 this.text4 = "TAP HERE TO EXIT";
                 this.text5 = "TAP HERE TO PLAY AGAIN";
+                this.text6 = "PRESS SPACE TO ENTER HIGHSCORE";
+                this.text7 = "TAP HERE TO ENTER HIGHSCORE";
 
                 if (me.device.isMobile) {
-                    this.widthItem = me.game.viewport.width - ((2.8 / 3) * me.game.viewport.width);
+   /*                 this.widthItem = me.game.viewport.width - ((2.8 / 3) * me.game.viewport.width);
                     this.height1 = 200;
                     this.height2 = 225;
                     this.height3 = 250;
                     this.height4 = 275;
-                    this.height5 = 300;
+                    this.height5 = 300;   */
                     this.font = fontMobile;
                 } else {
-                    this.widthItem = me.game.viewport.width - ((2.9 / 3) * me.game.viewport.width);
+      /*              this.widthItem = me.game.viewport.width - ((2.9 / 3) * me.game.viewport.width);
                     this.height1 = 350;
                     this.height2 = 400;
                     this.height3 = 450;
                     this.height4 = 500;
-                    this.height5 = 550;
+                    this.height5 = 550;   */
                     this.font = font;
-                }
+                }   
             },
 
             update: function(dt) {
@@ -64,21 +67,26 @@ game.ScoreScreen = me.ScreenObject.extend({
                 if (me.device.isMobile) {
                     this.font.draw(context, this.text4, me.game.viewport.width * (0.2 / 3), me.game.viewport.height * (2.7 / 3));
                     this.font.draw(context, this.text5, me.game.viewport.width * (2 / 3), me.game.viewport.height * (2.7 / 3));
+                    this.font.draw(context, this.text7, me.game.viewport.width * (0.8 / 3), me.game.viewport.height * (2.4 / 3));
                 } else {
                     this.font.draw(context, this.text2, me.game.viewport.width * (0.2 / 3), me.game.viewport.height * (2.7 / 3));
-                    this.font.draw(context, this.text3, me.game.viewport.width * (1.9 / 3), me.game.viewport.height * (2.7 / 3));               
+                    this.font.draw(context, this.text3, me.game.viewport.width * (1.9 / 3), me.game.viewport.height * (2.7 / 3));         
+                    this.font.draw(context, this.text6, me.game.viewport.width * (0.8 / 3), me.game.viewport.height * (2.4 / 3));      
                 }
             }
         })));
 
         me.input.bindKey(me.input.KEY.ENTER, "enter", true);
         me.input.bindKey(me.input.KEY.ESC, "escape", true);
+        me.input.bindKey(me.input.KEY.SPACE, "space", true);
         this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
             if (action === "enter") {
                 // play something on enter
                 me.state.change(me.state.PLAY);
             } else if (action === "escape") {
                 me.state.change(me.state.INFO);
+            } else if(action === "space"){
+                me.state.change(me.state.HIGHSCORE);
             }
         });   
     },
@@ -86,6 +94,7 @@ game.ScoreScreen = me.ScreenObject.extend({
     onDestroyEvent: function() {
         me.input.unbindKey(me.input.KEY.ENTER);
         me.input.unbindKey(me.input.KEY.ESC);
+        me.input.unbindKey(me.input.KEY.SPACE);
         me.event.unsubscribe(this.handler);
         if (me.device.isMobile) {
             me.game.world.removeChild(this.UI);
